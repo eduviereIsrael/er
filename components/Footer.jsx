5d5fd175f7973c45dfeb5a314c815f03488d5dbd
Eduviere Israel <eduviereisrael@gmail.com>
2023-02-26 16:02:55 +0100

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,21 @@
 /* eslint-disable */
 
-import Link from "next/link"
+import Link from "next/link";
+import SecondaryBtn from './SecondaryBtn';
+
+
 
 const footer = () => {
   return (
-    <div className='footer sect-wrap'>
-        <h2>ER</h2>
+    <div className='footer'>
+      <div className="container">
+        <div className="intro">
+          <span className="large-text">IT ALL STARTS WITH A CONVERSATION</span>
+          <p className="small-text">Let us know what you're looking for</p>
+          <p className="small-text">Say Hi on whatsapp</p>
+          <SecondaryBtn color="#fff" link="" text="Let's Chat" />
+        </div>
+      </div>
     </div>
   )
 }
