e9793c3a240ab443453e2f300ca370a77dbb8002
Eduviere Israel <eduviereisrael@gmail.com>
2023-03-14 04:55:29 +0100

    rendered mobile menu in the layout component

--- components/MainLayout.jsx
+++ components/MainLayout.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import MobileMenu from './MobileMenu';
+import { useStateContext } from '../Context/StateContext';
 
 const MainLayout = ({children}) => {
+  const {activeNavbar} = useStateContext();
   return (
     <div style={{width: '100%'}}>
       {activeNavbar? <MobileMenu/> : '' }
