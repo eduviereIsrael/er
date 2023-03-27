a2952069c79b68a1ab7b52950e4bac607992df54
Eduviere Israel <eduviereisrael@gmail.com>
2023-03-27 10:22:38 +0200

    perks copy updated

--- data/perks.js
+++ data/perks.js
@@ -1,18 +1,18 @@
 export const perks = [
     {
-        title: "Smooth Process",
-        text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam."
+        title: "Exceptional Customer Service",
+        text: " We believe that great service is the key to a successful transaction. At Ephrael Realty our team is dedicated to providing exceptional customer service. We'll keep you informed throughout the buying or selling process and provide the support you need to make informed decisions."
     },
     {
-        title: "Transparency",
-        text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam."
+        title: "Local Expertise",
+        text: "Our team has extensive knowledge of the local market. We'll provide you with valuable insights on market trends, neighborhoods, and school districts. Whether you're buying or selling, we have the local expertise you need to make a successful transaction."
     },
     {
-        title: "Rich Network",
-        text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam."
+        title: "Advanced Technology",
+        text: "We incorporate advanced technology to make the home buying or selling process more efficient and convenient for our clients. Our website offers virtual property tours, online scheduling for property visits, and digital document signing, making it easier for you to complete your transaction from anywhere."
     },
     {
-        title: "Smooth Process",
-        text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam."
+        title: "Transparent Communication",
+        text: "At Ephrael Realty, we believe in transparent communication. We keep our clients informed throughout the process, are accessible and responsive, and set realistic expectations. By providing open and honest communication, we build trust and establish long-term relationships with our clients."
     },
 ]
